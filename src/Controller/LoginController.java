d2448f58479e4bb160185a6c9c3e65b6ece614ca
GABRIEL LOFF SA DE MORAES <15103684@pucrs.br>
2019-04-01 20:19:59 -0300

    Tela Login

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,7 +1,13 @@
 package Controller;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController extends Application {
 
@@ -11,6 +17,14 @@ public class LoginController extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        primaryStage.setTitle("Hello World!");
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../View/Login.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
