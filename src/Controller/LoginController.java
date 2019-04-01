39870475a3433b74a529763e3dcd05d0e3fac913
GABRIEL LOFF SA DE MORAES <15103684@pucrs.br>
2019-04-01 20:39:03 -0300

    Tela Login #2

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,15 +1,21 @@
 package Controller;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ImageView;
 import java.io.IOException;
 
+import static com.sun.corba.se.impl.util.Utility.printStackTrace;
+
 public class LoginController extends Application {
+
 
     public static void main(String[] args) {
         launch(args);
@@ -24,7 +30,8 @@ public class LoginController extends Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root, 350, 500));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 }
