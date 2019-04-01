66651a9461800ede4b22b4a5af0c0a525b02066a
VITOR EDGAR DA SILVA <16204005@pucrs.br>
2019-04-01 20:40:12 -0300

    modals

---
+++ src/BancoMocado/BancoMocado.java
@@ -0,0 +1,25 @@
+package BancoMocado;
+
+import DTO.CompradorDTO;
+import DTO.ProdutoDTO;
+import DTO.VendedorDTO;
+
+import java.util.ArrayList;
+
+public class BancoMocado {
+
+    ArrayList<ProdutoDTO> produtos = new ArrayList<>();
+    ArrayList<CompradorDTO> compradores = new ArrayList<>();
+    ArrayList<VendedorDTO> vendedores = new ArrayList<>();
+
+    public void crudProduto(ProdutoDTO produto){
+        return;
+    }
+    public void crudComprador(CompradorDTO comprador){
+        return;
+    }
+    public void crudVendedor(VendedorDTO vendedor){
+        return;
+    }
+
+}
---
+++ src/Model/ProdutoModel.java
@@ -0,0 +1,9 @@
+package Model;
+
+import DTO.ProdutoDTO;
+
+public class ProdutoModel {
+
+
+
+}
